c90c5c584ce28260dab7874dea76c230bfc0e26d
dashori <daahaaa@icloud.com>
2023-03-19 16:59:37 +0300

    add shift

--- lisp/mankala/mankala.lisp
+++ lisp/mankala/mankala.lisp
@@ -1,8 +1,3 @@
-;; (defun play1 ()
-;;   (format t "Welcome to Mancala!~%")
-;; )
-
-
 (defun introduce ()
     (format t 
 "Чука Рума - редчайшая разновидность однорядной манкалы для одного игрока.~%
@@ -20,6 +15,8 @@
 
 (introduce)
 
+;; (defparameter *glob* resultLst)
+(defvar resultList ())
 
 (defun inputCountHoles ()
     (format t "Введите количество лунок: ")
@@ -41,6 +38,7 @@
 (defun againInputHole ()
     (format t "Количество камней в лунке не может быть отрицательным.")
     (finish-output)
+    (setf resultList (cons 1 resultList))
     T
 )
 
@@ -60,23 +58,132 @@
     (finish-output)
     (if (< tempCount needCount)
         (cons (inputStone tempCount) (inputStones (+ 1 tempCount) needCount stones))
-        stones
+        (nconc stones '(0))
+        ;; (cons stones (cons 0 nil))
     )
 )
 
+(defun tempPrint (stones)
+    (print stones)
+)
+
+(defun isWin (stones) ;; проверяем весь лист на нули, кроме последнего элемнта
+    (cond 
+          ((and (eql (car stones) 0) (cddr stones)) 
+                (isWin (cdr stones)))
+          ((not (eql (car stones) 0))  
+                NIL)
+          (T T)
+    )
+)
+
+(print (isWin '(1 2 3 4)))
+(print (isWin '(0 0 0 0)))
+(print (isWin '(0 0 0 1)))
+(print (isWin '(0 2 0 1)))
+(print (isWin '(0 0 0 0 0 1)))
+
+(defun findNotZeroHole (stones index) ;; ищем первый не 0 и чтоб это был не дом
+    (cond 
+        ((and (not (eql (car stones) 0)) (cdr stones))
+            index)
+        ((and (eql (car stones) 0) (cdr stones))
+            (findNotZeroHole (cdr stones) (+ 1 index)))
+        ((cdr stones)
+            NIL)
+    )
+)
+
+(write-line "not zero hole")
+(print (findNotZeroHole '(1 2 3 4 5) 0))
+(print (findNotZeroHole '(0 1 3 4 5) 0))  
+(print (findNotZeroHole '(0 0 3 4 5) 0))  
+(print (findNotZeroHole '(0 0 0 4 5) 0))  
+(print (findNotZeroHole '(0 0 0 0 5) 0))  
+(write-line "")
+
+(defun shift (size lst idx)  ;; функция для сдвига 
+  (let ((val (nth idx lst))
+        (newlst (copy-list lst))
+        (i (+ idx 1)))
+    (setf (nth idx newlst) 0)
+    (dotimes (j val)
+    ;;   (print newlst)
+      (setf (nth i newlst)
+            (+ 1 (nth i newlst)))
+      (setf i (mod (+ i 1) size)))
+    newlst
+    )
+)
+
+(defun whichHole (len stones i) ;; функция для расчета куда попадет сдвиг
+    (let ((i  (- i 1)))
+        ;; (print (nth i stones))
+        (if ( < (+ (- (nth i stones) len) i) 0)
+            (+ (nth i stones) i)
+            (mod (+ (- (nth i stones) len) i) len)
+        )
+    )
+)
+
+(write-line "WHICH HOLE")
+(print (whichHole 6 '(1 2 3 4 20 0) 5))
+(print (whichHole 6 '(1 2 3 4 21 0) 5))
+(print (whichHole 6 '(1 2 3 4 22 0) 5))
+(print (whichHole 6 '(1 2 3 4 23 0) 5))
+(print (whichHole 6 '(1 2 3 4 24 0) 5))
+(print (whichHole 6 '(1 2 3 4 25 0) 5))
+(print (whichHole 6 '(1 2 3 4 26 0) 5))
+
+(write-line "")
+
+
+;; (defun dop (len stones i)
+;;     (let ((resPos )))
+
+;; )
+
+(defun mainSolve (len stones)
+    (let ((hole (findNotZeroHole stones 0)))
+        (if hole
+            ;; (let (res (dop len stones hole))
+                (print hole)
+            ;; )
+            
+            (print "loh")
+        )
+    )
+)
+
+;; (defun solve (stones temp stonesBegin)
+;;     ()
+;; )
 
 (defun play ()
     (let ((holesCount (inputCountHoles)))
         (if holesCount
             (let ((stones  (inputStones 0 holesCount ())))
-                 (print stones)
+                ;; (cons stones '(0))
+                (tempPrint stones)
+                (mainSolve (length stones) stones)
             )
         )
     )
 
+    (print resultList)
 )
 
+;; (play)
 
-(play)
+(print (shift 5 '(1 3 3 4 0) 1))
+(print (shift 5 '(1 2 3 4 0) 1))
+(print (shift 4 '(1 2 3 4) 1))
+(print (shift 6 '(1 2 3 4 20 0) 4)) ;; 5 5 6 7 3 4 
+(print (shift 6 '(1 2 3 4 21 0) 4)) ;; 5 6 6 7 3 4 
+(print (shift 6 '(1 2 3 4 1 0) 4)) ;; 1 2 3 4 0 1
+(print (shift 6 '(1 2 3 4 1 0) 0)) ;; 0 3 3 4 1 0
 
-;; (print ((format t "Количество камней в лунке не может быть отрицательным.")))
+;; (print (length '(1 2 3 4 )))
+
+;; (print (cons '(1 2 3 4 5) '(0 nil) ))
+;; (print (cons '0 '(1 2 3 4 5) ))
