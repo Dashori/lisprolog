cd879d4f13735e89487bdd230adde34ae1d92cdd
dashori <daahaaa@icloud.com>
2023-03-20 17:22:20 +0300

    pretty fix

--- lisp/mankala/mankala.lisp
+++ lisp/mankala/mankala.lisp
@@ -19,7 +19,7 @@
 (defvar resultList ())
 
 (defun inputCountHoles ()
-    (format t "Введите количество лунок: ")
+    (format t "~%Введите количество лунок: ")
     (finish-output)
     (let ((holes (read)))
         ;; (when (not (numberp holes))
@@ -31,19 +31,17 @@
             (finish-output) 0)
             holes
         )
-
     )
 )
 
 (defun againInputHole ()
     (format t "Количество камней в лунке не может быть отрицательным.")
     (finish-output)
-    ;; (setf resultList (cons 1 resultList))
     T
 )
 
 (defun inputStone(hole)
-    (format t "Введите количество камней в ~A лунке: " (+ 1 hole))
+    (format t "Введите количество камней в лунке ~A: " (+ 1 hole))
     (finish-output)
     (let ((stone (read)))
         (if (> 0 stone)
@@ -77,11 +75,11 @@
     )
 )
 
-(print (isWin '(1 2 3 4)))
-(print (isWin '(0 0 0 0)))
-(print (isWin '(0 0 0 1)))
-(print (isWin '(0 2 0 1)))
-(print (isWin '(0 0 0 0 0 1)))
+;; (print (isWin '(1 2 3 4)))
+;; (print (isWin '(0 0 0 0)))
+;; (print (isWin '(0 0 0 1)))
+;; (print (isWin '(0 2 0 1)))
+;; (print (isWin '(0 0 0 0 0 1)))
 
 
 (defun resize (stones index)
@@ -102,49 +100,63 @@
     )
 )
 
-(write-line "not zero hole")
-(print (findNotZeroHole '(1 2 3 4 5) 0))
-(print (findNotZeroHole '(0 1 3 4 5) 0))  
-(print (findNotZeroHole '(0 0 3 4 5) 0))  
-(print (findNotZeroHole '(0 0 0 4 5) 0))  
-(print (findNotZeroHole '(0 0 0 0 5) 0))  
-(write-line "")
+;; (write-line "not zero hole")
+;; (print (findNotZeroHole '(1 2 3 4 5) 0))
+;; (print (findNotZeroHole '(0 1 3 4 5) 0))  
+;; (print (findNotZeroHole '(0 0 3 4 5) 0))  
+;; (print (findNotZeroHole '(0 0 0 4 5) 0))  
+;; (print (findNotZeroHole '(0 0 0 0 5) 0))  
+;; (write-line "")
 
-(write-line "not zero hole 2")
-(print ( + 0 (findNotZeroHole (resize '(1 2 3 4 5) 0) 0)))
-(print ( + 1 (findNotZeroHole (resize '(1 2 3 4 5) 1) 0)))  
-(print ( + 2 (findNotZeroHole (resize '(1 2 3 4 5) 2) 0)))
-(print ( + 3 (findNotZeroHole (resize '(1 2 3 4 5) 3) 0)))  
-(print ( findNotZeroHole (resize '(1 2 3 4 5) 4) 0))
-(write-line "")
-
-
-(write-line "resize")
-(print (resize '(1 2 3 4 5) 0))  
-(print (resize '(1 2 3 4 5) 1))  
-(print (resize '(1 2 3 4 5) 2))  
-(print (resize '(1 2 3 4 5) 3))  
-(print (resize '(1 2 3 4 5) 4))  
-(write-line "")
+;; (write-line "not zero hole 2")
+;; (print ( + 0 (findNotZeroHole (resize '(1 2 3 4 5) 0) 0)))
+;; (print ( + 1 (findNotZeroHole (resize '(1 2 3 4 5) 1) 0)))  
+;; (print ( + 2 (findNotZeroHole (resize '(1 2 3 4 5) 2) 0)))
+;; (print ( + 3 (findNotZeroHole (resize '(1 2 3 4 5) 3) 0)))  
+;; (print ( findNotZeroHole (resize '(1 2 3 4 5) 4) 0))
+;; (write-line "")
 
 
-(defun shift (size lst idx)  ;; функция для сдвига 
-  (let ((val (nth idx lst))
-        (newlst (copy-list lst))
-        (i (+ idx 1)))
-    (setf (nth idx newlst) 0)
-    (dotimes (j val)
-    ;;   (print newlst)
-      (setf (nth i newlst)
-            (+ 1 (nth i newlst)))
-      (setf i (mod (+ i 1) size)))
-    newlst
+;; (write-line "resize")
+;; (print (resize '(1 2 3 4 5) 0))  
+;; (print (resize '(1 2 3 4 5) 1))  
+;; (print (resize '(1 2 3 4 5) 2))  
+;; (print (resize '(1 2 3 4 5) 3))  
+;; (print (resize '(1 2 3 4 5) 4))  
+;; (write-line "")
+
+(defun copyList (stones)
+    (if (atom stones)
+        stones
+        (cons (car stones) (copyList (cdr stones)))
     )
 )
 
+(defun shift (len stones index)  ;; функция для сдвига 
+  (let ((val (nth index stones))
+        (newlist (copyList stones))
+        (i (+ index 1)))
+    (setf (nth index newlist) 0)
+    (dotimes (j val)
+      (setf (nth i newlist)
+            (+ 1 (nth i newlist)))
+      (setf i (mod (+ i 1) len)))
+    newlist
+    )
+)
+
+;; (print (shift 5 '(1 3 3 4 0) 1)) ;; 1 0 4 5 1
+;; (print (shift 5 '(1 2 3 4 0) 1)) ;; 1 0 4 5 0 
+
+;; (print (shift 4 '(1 2 3 4) 1))
+;; (print (shift 6 '(1 2 3 4 20 0) 4)) ;; 5 5 6 7 3 4 
+;; (print (shift 6 '(1 2 3 4 21 0) 4)) ;; 5 6 6 7 3 4 
+;; (print (shift 6 '(1 2 3 4 1 0) 4)) ;; 1 2 3 4 0 1
+;; (print (shift 6 '(1 2 3 4 1 0) 0)) ;; 0 3 3 4 1 0
+
+
 (defun whichHole (len stones i) ;; функция для расчета куда попадет сдвиг
     (let ((i  (- i 1)))
-        ;; (print (nth i stones))
         (if ( < (+ (- (nth i stones) len) i) 0)
             (+ (nth i stones) i)
             (mod (+ (- (nth i stones) len) i) len)
@@ -152,144 +164,86 @@
     )
 )
 
+;; (write-line "WHICH HOLE")
+;; (print (whichHole 6 '(1 2 3 4 20 0) 4))
+;; (print (whichHole 6 '(1 2 3 4 21 0) 4))
+;; (print (whichHole 6 '(1 2 3 4 22 0) 4))
+;; (print (whichHole 6 '(1 2 3 4 23 0) 4))
+;; (print (whichHole 6 '(1 2 3 4 24 0) 4))
+;; (print (whichHole 6 '(1 2 3 4 25 0) 4))
+;; (print (whichHole 6 '(1 2 3 4 26 0) 4))
 
 
-(write-line "WHICH HOLE")
-(print (whichHole 6 '(1 2 3 4 20 0) 4))
-(print (whichHole 6 '(1 2 3 4 21 0) 4))
-(print (whichHole 6 '(1 2 3 4 22 0) 4))
-(print (whichHole 6 '(1 2 3 4 23 0) 4))
-(print (whichHole 6 '(1 2 3 4 24 0) 4))
-(print (whichHole 6 '(1 2 3 4 25 0) 4))
-(print (whichHole 6 '(1 2 3 4 26 0) 4))
+;; (write-line "")
+;; (print (whichHole 5 '(2 2 2 2 0) 1))
+;; (print (whichHole 5 '(0 3 3 2 0) 3))
+;; (write-line "")
 
 
-(write-line "")
-(print (whichHole 5 '(2 2 2 2 0) 1))
-(print (whichHole 5 '(0 3 3 2 0) 3))
-(write-line "")
-
-
-(defun mainSolve (len stones index resizeParam)
-    (print "MAIN SOLVE")
-    (print resizeParam)
-    (let ((hole (findNotZeroHole (resize stones resizeParam) index)))
-    ;; (let ((hole (findNotZeroHole stones index)))
-        ;; (print hole)
+(defun mainSolve (len stones index resizeParam) 
+    (let ((hole (findNotZeroHole (resize stones resizeParam) index))) ;; ищем первую непустую лунку
         (if hole
-            (let ((res (dop len stones (+ resizeParam hole))))
-        ;;         (print "in main solve")
-        ;;         (print hole)
-                (print "RES")
-                (print res)
-
+            (let ((res (dop len stones (+ resizeParam hole))))        ;; пытаемся начать с нее игру 
                 (if (not res)
-                    (mainSolve len stones index (+ 1 resizeParam))
-                    ;; (progn 
+                    (mainSolve len stones index (+ 1 resizeParam))   
                     (setf resultList (cons (+ 1 resizeParam hole) resultList))
-                    ;; T)
-                    ;; T
                 )
             )
             NIL
-            ;; (print "loh")
         )
-        ;; (print "a")
     )
 )
 
 (defun dop (len stones i)
-    (print "DOP")
-    (print len)
-    (let ((resPos (whichHole len stones (+ i 1)))
-        (new_stones (shift (length stones) stones i)))
-
-        (print stones)
-        (print new_stones)
-        (print resPos)
-        
+    (let ((resPos (whichHole len stones (+ i 1)))           ;; ищем в какую лунку попали
+        (new_stones (shift (length stones) stones i)))      ;; состояние лунок после хода
         (if (isWin new_stones)
-            (print "WIN")
-            ;; NIL
-        ;; )
-        (cond 
-                ((eql resPos (- len 1))   ;; дом 
-                    (print "HOME ")
-                    (if (mainSolve len new_stones 0 0)
-                        T
-                        NIL
+            T
+            (cond 
+                    ((eql resPos (- len 1))   ;; дом => опять в мейн солв
+                        (if (mainSolve len new_stones 0 0)
+                            T
+                            NIL
+                        )
                     )
-                )
-                ((eql (nth resPos new_stones) 1)
-                    NIL) 
-                (T 
-                    (let ((resDop (dop len new_stones resPos)))
-                        (if resDop
-                            ;; T
-                            (setf resultList (cons (+ 1 resPos) resultList))
-                            NIL)))
-
-                ;; (print "T"))
+                    ((eql (nth resPos new_stones) 1) ;; попали в пустую и вышли
+                        NIL) 
+                    (T                              ;; берем камни из лунки, в которую попали и снова ходим
+                        (let ((resDop (dop len new_stones resPos))) 
+                            (if resDop
+                                (setf resultList (cons (+ 1 resPos) resultList))
+                                NIL)))
+            )
         )
-        )
-        ;; NIL
-        ;; (print resPos)
-        ;; (print stones)
-        ;; (print new_stones)
-        ;; T
-        ;; (print (shift (length stones) stones (+ 1 i)))
-        ;; resPos
     )
 )
 
-;; (defun solve (stones temp stonesBegin)
-;;     ()
-;; )
+(defun printResult (stones resultList)
+    (when resultList
+        (let ((newStones (shift (length stones) stones (- (car resultList) 1))))
+                (format t "~%Ход из лунки ~A: ~A -> ~A" (car resultList) stones newStones)
+                (printResult newStones (cdr resultList))
+        )
+    )
+)
 
 (defun play ()
     (let ((holesCount (inputCountHoles)))
         (if holesCount
             (let ((stones  (inputStones 0 holesCount ())))
-                ;; (cons stones '(0))
+                (format t "~%Начальное состояние доски: ")
+                (finish-output)
                 (tempPrint stones)
-                (let ((result (mainSolve (length stones) stones 0 0)))
-                    (write-line "")
-                    (print "!!!RESULT!!!")
-                    (print result)
+                (mainSolve (length stones) stones 0 0)
+                (format t "~%~%Решение:")
+                (if resultList
+                    (printResult stones resultList)
+                    (format t "~%Пасьянс не собрался :(")
                 )
             )
         )
     )
-
-    (print resultList)
 )
-
-
-
-;; (print (shift 5 '(1 3 3 4 0) 1))
-;; (print (shift 5 '(1 2 3 4 0) 1))
-;; (print (shift 4 '(1 2 3 4) 1))
-;; (print (shift 6 '(1 2 3 4 20 0) 4)) ;; 5 5 6 7 3 4 
-;; (print (shift 6 '(1 2 3 4 21 0) 4)) ;; 5 6 6 7 3 4 
-;; (print (shift 6 '(1 2 3 4 1 0) 4)) ;; 1 2 3 4 0 1
-;; (print (shift 6 '(1 2 3 4 1 0) 0)) ;; 0 3 3 4 1 0
-
-;; (print (length '(1 2 3 4 )))
-
-;; (print (cons '(1 2 3 4 5) '(0 nil) ))
-;; (print (cons '0 '(1 2 3 4 5) ))
 
 (play)
 
-
-;; (defun try (lst) 
-;;     (let ((a 5))
-;;         (if lst
-;;             T
-;;             NIL)
-;;         ;; (print "b")
-;;     )
-;;     (print "a")
-;; )
-
-;; (print (try '(1 2 3)))
